015712c76006e36daa93e314b66b7987c3f74ce6
OxRobot <elhanafiabbaali@gmail.com>
2026-03-19 04:31:42 +0000

    Allow external image domains in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,30 @@ const nextConfig: NextConfig = {
         protocol: 'https',
         hostname: 'images.unsplash.com',
       },
+      {
+        protocol: 'https',
+        hostname: 'media.ferrari.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'media.lamborghini.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'files.porsche.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'robbreport.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.motortrend.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'hips.hearstapps.com',
+      }
     ],
   },
 };
